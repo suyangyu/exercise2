1da6153b1d478bc4453a90cb20212c03f71dfda0
Su-Yang Yu <su_yangyu@hotmail.com>
2016-11-07 20:01:07 +0000

    type changes

--- src/main/scala-2.11/units/MenuItem.scala
+++ src/main/scala-2.11/units/MenuItem.scala
@@ -3,31 +3,41 @@ package units
 sealed trait MenuItem {
   def name: String
 
+  def tempCategory: TempCategory
+
   def price: Double
 
   override def toString = name
 }
 
-case object Cola extends MenuItem with Cold {
+case object Cola extends MenuItem {
   def name: String = "Cola"
+
+  def tempCategory: TempCategory = Cold
 
   def price: Double = 0.5
 }
 
-case object Coffee extends MenuItem with Hot {
+case object Coffee extends MenuItem {
   def name: String = "Coffee"
+
+  def tempCategory: TempCategory = Hot
 
   def price: Double = 1.0
 }
 
-case object CheeseSandwich extends MenuItem with Cold {
+case object CheeseSandwich extends MenuItem {
   def name: String = "Cheese Sandwich"
+
+  def tempCategory: TempCategory = Cold
 
   def price: Double = 2.0
 }
 
-case object SteakSandwich extends MenuItem with Hot {
+case object SteakSandwich extends MenuItem {
   def name: String = "Steak Sandwich"
+
+  def tempCategory: TempCategory = Hot
 
   def price: Double = 4.5
 }
